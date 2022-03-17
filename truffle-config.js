15219e0427d45aac6dfbc26f1449d251b1bf59f3
Vaibhav Tomar <vaibhavtomar728@gmail.com>
2022-03-17 15:28:17 +0530

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,25 @@
+const mnemonic = process.env.MNEMONIC;
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,     
+      network_id: "*" // Match any network id
+    },
+    polygon: {
+      provider: new HDWalletProvider(mnemonic, process.env.POLYGON_RPC),
+      network_id: 137,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    mumbai: {
+      provider: new HDWalletProvider(mnemonic, process.env.POLYGON_MUMBAI_RPC),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    }
